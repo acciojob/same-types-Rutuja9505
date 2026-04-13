c454fe6b9b2f7dde87f5391c250a2333c34c21c1
Rutuja9505 <rutujabiradar9505@gmail.com>
2026-04-13 10:43:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function isSameType(value1, value2) {
+   
+    if (Number.isNaN(value1) && Number.isNaN(value2)) {
+        return true;
+    }
+
+    
+    if (Number.isNaN(value1) || Number.isNaN(value2)) {
+        return false;
+    }
+
     return typeof value1 === typeof value2;
 }
 
@@ -6,5 +16,7 @@ function isSameType(value1, value2) {
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
+
+
 
 
